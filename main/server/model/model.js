0f6f670fcc6d47d95bf7df394a507f7fe9cc56ef
Manuel Mauky <manuel.mauky@googlemail.com>
2014-04-08 19:30:26 +0200

    Data Model and test

--- main/server/model/model.js
+++ main/server/model/model.js
@@ -1,28 +1,167 @@
+"use strict"
 
+
+/**
+ * The size that the board can have.
+ *
+ * @type {{SMALL: number, BIG: number}}
+ */
+var BoardSize = {
+    SMALL: 7,
+    BIG: 9
+}
+
+/**
+ * The possible color of a figure.
+ *
+ * @type {{BLACK: string, WHITE: string}}
+ */
 var Color = {
-    BLACK : "black",
-    WHITE : "white"
+    BLACK: "black",
+    WHITE: "white"
 }
 
+/**
+ * The types of figures that are possible.
+ *
+ * @type {{ROCKS: string, MAN: string, WOMAN: string, KNIGHT: string, KNOWLEDGE: string, FAITH: string, ZENITH: string}}
+ */
 var FigureType = {
-    ROCKS : "rocks",
-    MAN : "man",
-    WOMAN : "woman",
-    JUMPER : "jumper"
+    ROCKS: "rocks",
+    MAN: "man",
+    WOMAN: "woman",
+    KNIGHT: "knight",
+    KNOWLEDGE: "knowledge",
+    FAITH: "faith",
+    ZENITH: "zenith"
 }
 
-var Figure = function(json){
-    "use strict";
-    this.id;
+/**
+ * The states that the match can have.
+ *
+ * @type {{READY: string, PAUSE: string, FINISHED: string, PLAYING: string}}
+ */
+var State = {
+    READY: "ready",
+    PAUSE: "pause",
+    FINISHED: "finished",
+    PLAYING: "playing"
+}
 
+/**
+ * The Figure constructor. This the figures of the game.
+ * Every figure is defined by its color and type (see
+ *
+ *
+ * @param json a json representation of the figure.
+ * @returns {Figure}
+ * @constructor
+ */
+var Figure = function (json) {
     var json = json || 0;
 
-    this.color= json.color || "";
-    this.figureType = json.figureType || "";
+    this.color = json.color;
+    this.figureType = json.figureType;
 
     return this;
 }
 
-module.exports.Figure = Figure;
+/**
+ * The Field constructor. This represents a single field of the chess board.
+ * A field is defined by its file (column) and rank (row) on the board.
+ * Every field can have zero or one figure on it.
+ *
+ * @param json
+ * @returns {Field}
+ * @constructor
+ */
+var Field = function (json) {
+    var json = json || 0;
+
+    this.figure = json.figure || undefined;
+
+    this.file = json.file;
+    this.rank = json.rank;
+
+    return this;
+}
+
+/**
+ * The Snapshot constructor. This represents a single point in time of the match.
+ *
+ *
+ * @param json
+ * @returns {Snapshot}
+ * @constructor
+ */
+var Snapshot = function (json) {
+
+    var json = json || 0;
+
+    this.board = new Array();
+    if (Array.isArray(json.board)) {
+        // for every json-entry create a field instance.
+        json.board.forEach(function (entry) {
+            this.board.push(new Field(entry));
+        }, this);
+    }
+
+    return this;
+}
+
+/**
+ * The player constructor.
+ *
+ * @param json
+ * @returns {Player}
+ * @constructor
+ */
+var Player = function (json) {
+    var json = json || 0;
+
+    this.playerId = json.playerId;
+    this.name = json.name;
+
+    return this;
+}
+
+/**
+ * The match constructor. A match represents the whole game with
+ * all data that is needed to describe the match incl. the history of the match.
+ *
+ * @param json
+ * @returns {Match}
+ * @constructor
+ */
+var Match = function (json) {
+    var json = json || 0;
+
+    this.playerWhite = json.playerWhite;
+    this.playerBlack = json.playerBlack;
+
+    this.matchId = json.matchId;
+    this.state = json.state || State.READY;
+
+    this.history = new Array();
+    // for every json-entry create a snapshot instance.
+    if (Array.isArray(json.history)) {
+        json.history.forEach(function (entry) {
+            this.history.push(new Snapshot(entry));
+        }, this);
+    }
+
+    this.size = json.size || BoardSize.SMALL;
+
+    return this;
+}
+
+
+module.exports.BoardSize = BoardSize;
 module.exports.Color = Color;
 module.exports.FigureType = FigureType;
+module.exports.State = State;
+module.exports.Figure = Figure;
+module.exports.Field = Field;
+module.exports.Snapshot = Snapshot;
+module.exports.Player = Player;
+module.exports.Match = Match;
--- main/server/model/model.test.js
+++ main/server/model/model.test.js
@@ -1,26 +1,30 @@
+"use strict"
 
-var assert = require("assert");
+
+var assert = require("chai").assert;
 var model = require("./model");
 
 
-describe("Creation of Figure", function(){
-    "use strict";
+var BoardSize = model.BoardSize;
+var Color = model.Color;
+var FigureType = model.FigureType;
+var Figure = model.Figure;
+var Field = model.Field;
+var Player = model.Player;
+var State = model.State;
+var Match = model.Match;
+var Snapshot = model.Snapshot;
 
-    var Figure = model.Figure;
-    var Color = model.Color;
-    var FigureType = model.FigureType;
+describe("Creation of Figure", function(){
 
     it("should be possible to with empty constructor", function(){
 
-
         var rocks = new Figure();
 
-
         assert.ok(rocks);
-        assert.equal(rocks.color, "");
-        assert.equal(rocks.figureType,"");
-
-    })
+        assert.notOk(rocks.color);
+        assert.notOk(rocks.figureType);
+    });
 
 
     it("should be possible with constructor params", function(){
@@ -30,7 +34,94 @@ describe("Creation of Figure", function(){
         assert.ok(man);
         assert.equal(man.color, Color.BLACK);
         assert.equal(man.figureType, FigureType.MAN);
+    });
 
-    })
+})
+
+describe("Creation of Match", function(){
+
+    it("should be possible with empty constructor", function(){
+        var match = new Match();
+
+        assert.ok(match);
+
+        assert.isArray(match.history);
+        assert.equal(match.history.length, 0);
+    });
+
+    it("should be possible from json", function(){
+
+        var json = {
+
+            matchId : "12345",
+
+            playerWhite: {
+                playerId: "102",
+                name: "max mustermann"
+            },
+            playerBlack: {
+                playerId: "104",
+                name: "john doe"
+            },
+
+            state: "playing",
+
+            size : 7,
+            history: [
+                {
+                    board:[
+                        {
+                            figure: {
+                                color: "black",
+                                figureType: "rocks"
+                            },
+                            file: 0,
+                            rank: 0
+                        },
+                        {
+                            file: 0,
+                            rank: 1
+                        },
+                        {
+                            file: 1,
+                            rank: 1
+                        }
+                    ]
+                }
+            ]
+        };
+
+        var match = new Match(json);
+
+        assert.ok(match);
+
+        assert.equal(match.matchId, "12345");
+
+        var max = new Player({playerId:"102", name:"max mustermann"});
+        var john = new Player({playerId:"104", name:"john doe"});
+
+        assert.deepEqual(match.playerWhite,max);
+        assert.deepEqual(match.playerBlack, john);
+
+        assert.equal(match.state, State.PLAYING);
+
+        assert.equal(match.size,7);
+
+
+        assert.isArray(match.history);
+        assert.equal(match.history.length, 1);
+
+
+        var rocks = new Figure({color:"black", figureType:"rocks"});
+
+        var snapshot = match.history[0];
+        assert.ok(snapshot);
+        assert.isArray(snapshot.board);
+        assert.equal(snapshot.board.length,3);
+
+        assert.deepEqual(snapshot.board[0], new Field({figure:rocks, file:0, rank:0}));
+        assert.deepEqual(snapshot.board[1], new Field({file:0, rank:1}));
+        assert.deepEqual(snapshot.board[2], new Field({file:1, rank:1}));
+    });
 
 })
