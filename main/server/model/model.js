6ebc15a09c00dc2d8aa577bb8e655f6f24a326a2
Manuel Mauky <manuel.mauky@googlemail.com>
2014-05-14 13:14:42 +0200

    Fixed build breaker

--- main/server/model/model.js
+++ main/server/model/model.js
@@ -279,7 +279,7 @@ var Match = function (json) {
      * @returns the Color of the active Player
      */
     this.getColorOfActivePlayer = function(){
-        if(match.history.length % 2 == 0){
+        if(this.history.length % 2 == 0){
             return Color.BLACK;
         }
         else return Color.WHITE;
