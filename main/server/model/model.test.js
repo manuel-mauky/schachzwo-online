32aa12d70729b3cd20fb86e56d02388d3dec242a
Erik <s3erjaeh@stud.hszg.de>
2014-05-14 11:34:04 +0200

    isValidMove und tests hinzugefügt

--- main/server/model/model.test.js
+++ main/server/model/model.test.js
@@ -306,10 +306,6 @@ describe("Match", function(){
             from: {column: 2, row: 5},
             to: {column: 2, row: 4}};
 
-        var invalidMove = {figure: {color: model.Color.BLACK, type: model.FigureType.ROCKS},
-            from: {column: 2, row: 5},
-            to: {column: 3, row: 4}};
-
         beforeEach(function(){
             match  = modelFactory.createEmptyMatch(BoardSize.SMALL);
         });
@@ -337,15 +333,6 @@ describe("Match", function(){
 
             assert.equal(match.history.length, 0);
         })
-
-        it.skip("should not add an invalid validMove", function(){
-            var result = match.addMove(invalidMove);
-
-            assert.isFalse(result);
-
-            assert.equal(match.history.length, 0);
-        })
-
     });
 
 
