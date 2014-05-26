64d695e3bbc8d256542b6bc4a759091199ede9d9
Paul Weinhold <weinpau@web.de>
2014-05-26 13:42:37 +0200

    Cache Filter deaktiviert

--- main/app.js
+++ main/app.js
@@ -9,14 +9,15 @@ var cookieParser = require("cookie-parser");
 app.use(bodyParser());
 app.use(cookieParser());
 
-
+/*
+IE compatibility
 app.use(function(req, res, next) {
     res.header('Pragma', 'no-cache');
     res.header('Cache-Control', 'no-store, no-cache, must-revalidate');
     res.header('Expires', 'Thu, 01 Jan 1970 00:00:00');
     next();
 });
-
+*/
 
 app.use("/matches", require("./server/routes/matches").route);
 
