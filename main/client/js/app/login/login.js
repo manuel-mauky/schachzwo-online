50d9d09edc09a3827b09e18acbe9392b3df9b543
Paul Weinhold <weinpau@web.de>
2014-06-09 10:41:51 +0200

    Clientseitige Login-Validierung angepasst

--- main/client/js/app/login/login.js
+++ main/client/js/app/login/login.js
@@ -11,27 +11,27 @@ define(['angular', 'jquery', 'jquery-validate'], function (angular, $) {
                 $('#login-form').validate({
                     errorClass: 'help-block animation-slideDown',
                     errorElement: 'span',
-                    errorPlacement: function(error, e) {
+                    errorPlacement: function (error, e) {
                         e.parents('.form-group > div').append(error);
                     },
-                    highlight: function(e) {
+                    highlight: function (e) {
                         $(e).closest('.form-group').removeClass('has-success has-error').addClass('has-error');
                         $(e).closest('.help-block').remove();
                     },
-                    success: function(e) {
+                    success: function (e) {
                         e.closest('.form-group').removeClass('has-success has-error');
                         e.closest('.help-block').remove();
                     },
                     rules: {
                         playerName: {
                             required: true,
-                            maxlength: 50
+                            maxlength: 20
                         }
                     },
                     messages: {
                         playerName: {
                             required: ' ',
-                            maxlength: 'Der Spielername darf nicht aus mehr als 50 Zeichen bestehen'
+                            maxlength: 'Der Spielername darf nicht aus mehr als 20 Zeichen bestehen'
                         }
                     }
                 });
@@ -45,17 +45,18 @@ define(['angular', 'jquery', 'jquery-validate'], function (angular, $) {
                 });
 
                 $scope.login = function () {
-                    $cookies.schachZwoPlayerName =  $scope.playerName;
-                    console.log("login : " + matchId);
-                    $http.post(endpoint + "/" + matchId + "/login", {name: $scope.playerName})
-                        .success(function (player) {
-                            $location.path("/match/" + matchId);
-                        })
-                        .error(function (data, status, headers, config) {
-                            console.log("error:" + status);
-                            $location.path("/500");
-                        });
-
+                    if ($('#login-form').valid()) {
+                        $cookies.schachZwoPlayerName = $scope.playerName;
+                        console.log("login : " + matchId);
+                        $http.post(endpoint + "/" + matchId + "/login", {name: $scope.playerName})
+                            .success(function (player) {
+                                $location.path("/match/" + matchId);
+                            })
+                            .error(function (data, status, headers, config) {
+                                console.log("error:" + status);
+                                $location.path("/500");
+                            });
+                    }
                 };
 
             }]);
